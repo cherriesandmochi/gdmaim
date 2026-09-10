12d3d754c4cebf23a201ced74f0d27b5377322b4
CNT <aid.me.service@gmail.com>
2026-09-10 00:01:07 -0300

    Case value
    - Added new lib value.

--- addons/gdmaim/export_plugin.gd
+++ addons/gdmaim/export_plugin.gd
@@ -306,11 +306,12 @@ func _export_end() -> void:
 		Engine.get_main_loop().create_timer(3.0).timeout.connect(_clean_libs.bind(_export_path))
 
 
+
 func _clean_libs(target_path : String) -> void:
 	if target_path.is_empty():
 		return
 		
-	const LIBS_NAME : PackedStringArray = ["libgdbc", "gdbc"]
+	const LIBS_NAME : PackedStringArray = ["libgdbc", "gdbc", "gdshedor"]
 	const EXTENSION : PackedStringArray = ["dll", "a", "wasm", "dylib", "so"]
 		
 	var sweet_msg : bool = true
@@ -320,7 +321,7 @@ func _clean_libs(target_path : String) -> void:
 		var da : DirAccess = DirAccess.open(dir)
 		if da:
 			da.list_dir_begin()
-			var file_name : String = da.get_next()
+			var file_name : String = da.get_next().to_lower()
 			var queue : PackedStringArray = []
 			
 			while file_name != "":
@@ -330,7 +331,7 @@ func _clean_libs(target_path : String) -> void:
 					
 					if vstart in LIBS_NAME and vend in EXTENSION:
 						queue.append(file_name)
-				file_name = da.get_next()
+				file_name = da.get_next().to_lower()
 			
 			var msg : String = ""
 			for q : String in queue:
@@ -342,7 +343,6 @@ func _clean_libs(target_path : String) -> void:
 			
 			if sweet_msg and !msg.is_empty():
 				print("[GDMaim] internal lib file/s cleaned",msg)
-
 
 
 func _export_file(path : String, type : String, features : PackedStringArray) -> void:
